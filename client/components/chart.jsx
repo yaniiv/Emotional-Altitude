246c1ce80385d06675201ab0efb583de13075817
yantonsoup <ygoldobin@gmail.com>
2016-11-10 17:52:01 -0800

    finish functional emotion graph

--- client/components/chart.jsx
+++ client/components/chart.jsx
@@ -13,31 +13,46 @@ import moment from 'moment';
 export default class Victory extends React.Component {
 	render() {
 		let dateTicks = this.props.emotionData.map((feelsObj) => {
-			return moment(feelsObj.feelingDate).format('LTS')
+			return moment(feelsObj.feelingDate).calendar()
 		});
 
 		let emotionTicks = this.props.emotionData.map((feelsObj) => {
-			return feelsObj.feelingNum
+			return feelsObj.feelingNum;
 		})
 
 		console.log('dateTicks ', dateTicks);
-		console.log('dateTicks ', dateTicks);
+		console.log('emotionTicks ', emotionTicks);
 		console.log('emotionData, ', this.props.emotionData);
 
 		return (		
-			<svg width={600} height={600}>
-				
+			<VictoryChart   >
+			<svg width={600} height={600}> 
+
 				<VictoryAxis
-					tickValues={dateTicks}
-					label={"Emotional Altitude"} 
+					tickValues={dateTicks}	
+					label={"Date"}
 				/>
-				
-				<VictoryAxis
-					dependentAxis
-					tickValues={emotionTicks}
-					label={"Date"} 
-				/>
-				
+
+				<VictoryScatter
+					animate={{duration: 2000}}	
+          style={{
+          data: {
+              fill: "teal",
+          },
+          labels: {
+              fill: "teal",
+              fontSize: 14,
+              padding: 12
+          }
+          }}
+          size={2}     
+					data={this.props.emotionData}
+					labels={(data)=>data.feelingText}
+					x={"feelingDate"}
+					y={"feelingNum"}
+        />  
+
+
 				<VictoryArea
 			  	animate={{duration: 2000}}
         	style={{
@@ -50,12 +65,21 @@ export default class Victory extends React.Component {
 					x={"feelingDate"}
 					y={"feelingNum"}
 				/>
-				
-				</svg>
+			</svg>
+			</VictoryChart>
 		)
 	}
 }
 
+								
+				// <VictoryAxis
+				// dependentAxis		
+				// 	orientation='left'	
+				// 	label={"Emotional Altitude"}
+				// 	tickCount={1}	
+				// 	// tickValues={[]}
+				// 	// tickFormat={["first", "second", "third"]}
+				// />
 
 
 
