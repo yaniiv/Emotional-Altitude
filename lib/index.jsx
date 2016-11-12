2cd9e1f5189c1f642d9fbd688c449d2781476b84
yantonsoup <ygoldobin@gmail.com>
2016-11-11 16:10:02 -0800

    make stateful header with redux

--- lib/index.jsx
+++ lib/index.jsx
@@ -1,32 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './pages/landing/login.jsx';
-import Home from './pages/home/home.jsx';
+
+import HomeContainer from './pages/home/HomeContainer.jsx';
+import HeaderContainer from './pages/header/HeaderContainer.jsx';
+
+
 import Signup from './landing/signup/signup.jsx';
 import Navbar from './landing/signup/nav-bar.jsx';
 import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
 
+
+
 import { Provider } from 'react-redux';
 import { store } from './store.js';
 
-class App extends React.Component {
-  constructor() {
-    super();
-  }
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//   }
 
-  render() {
-    return (
-      <div>
-        <Navbar />
-        {React.cloneElement(this.props.children, this.props)}
-      </div>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <Navbar />
+//         {React.cloneElement(this.props.children, this.props)}
+//       </div>
+//     )
+//   }
+// }
 
 ReactDOM.render((
   <Provider store={store}>
-
+    <div>
+    <HeaderContainer/>
+    <HomeContainer/>
+    </div>
   </Provider>
 ), document.getElementById('content'));
 
---
+++ lib/pages/header/HeaderContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class HeaderContainer extends React.Component {
+  constructor(){
+    super()
+  }
+
+  render(){
+   return {
+     <div className="header">
+        hihihi
+      </div>
+   }
+  }
+}
+
