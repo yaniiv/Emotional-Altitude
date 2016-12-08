20c3a957356babb8d47a073ac610dc471b926a79
yantonsoup <ygoldobin@gmail.com>
2016-12-07 17:43:01 -0800

    install blueprint for UI

--- lib/pages/home/HomeContainer.jsx
+++ lib/pages/home/HomeContainer.jsx
@@ -5,37 +5,29 @@ import d3 from 'd3';
 import $ from 'jquery';
 import Victory from './chart.jsx';
 import CommentForm from './comment_form.jsx';
-import { Router, Route, Link } from 'react-router';
+import {Router, Route, Link} from 'react-router';
 
-import { connect } from 'react-redux'; 
+import {connect} from 'react-redux';
 import store from '../../store.js';
 import * as types from '../../actions/action-types.js';
-
+import { Colors, Navbar, Spinner, DatePickerFactory} from "@blueprintjs/core";
 
 class HomeContainer extends React.Component {
   constructor() {
     super();
-    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+    this.handleCommentSubmit = this
+      .handleCommentSubmit
+      .bind(this);
   }
 
-  // OLDhandleCommentSubmit(e) {
-  //   e.preventDefault();
-  //   console.log('feeling data before adding new feels: \n', this.state.data);
-
-  //   const feeling = {
-  //     feelingNum: e.target.elements[0].valueAsNumber,
-  //     feelingText: e.target.elements[1].value,
-  //     feelingDate: new Date()
-  //   };
-  //   console.log("new feeling data point to add", feeling);
-
-  //   const feelingData = this.state.data;
-  //   feelingData.push(feeling);
-  //   this.setState({ data: feelingData });
-
-  //   console.log('feeling data after adding new feels: \n', this.state.data);
-  // }
-
+  // OLDhandleCommentSubmit(e) {   e.preventDefault();   console.log('feeling data
+  // before adding new feels: \n', this.state.data);   const feeling = {
+  // feelingNum: e.target.elements[0].valueAsNumber,     feelingText:
+  // e.target.elements[1].value,     feelingDate: new Date()   };
+  // console.log("new feeling data point to add", feeling);   const feelingData =
+  // this.state.data;   feelingData.push(feeling);   this.setState({ data:
+  // feelingData });   console.log('feeling data after adding new feels: \n',
+  // this.state.data); }
 
   handleCommentSubmit(e) {
     e.preventDefault();
@@ -49,34 +41,31 @@ class HomeContainer extends React.Component {
     })
   }
 
-
-  // postDataToDB(feeling) {
-  //   $.ajax({
-  //     type: "POST",
-  //     url: "http://localhost:7777/postUserData",
-  //     data: userInfo,
-  //     success: function () {
-  //       console.log('success')
-  //     }
-  //   })
-  // }
-
+  // postDataToDB(feeling) {   $.ajax({     type: "POST",     url:
+  // "http://localhost:7777/postUserData",     data: userInfo,     success:
+  // function () {       console.log('success')     }   }) }
 
   render() {
     return (
-      <div className="commentBox">
-      hihihihi
-        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
-      <Victory className="chart" emotionData={this.props.data} />
+      <div>
+      
+
+        <div className="commentBox">
+
+          <button className="pt-button @blue3">dawda</button>
+          
+          hihihihi
+          <Spinner/>
+          <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
+          <Victory className="chart" emotionData={this.props.data}/>
         </div>
-      )
+      </div>
+    )
   }
 }
 
-const mapStateToProps = function(store){
-  return {
-    data: store.homeState.data
-  }
+const mapStateToProps = function (store) {
+  return {data: store.homeState.data}
 }
 
 export default connect(mapStateToProps)(HomeContainer)
