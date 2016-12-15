7321dd3d256bd04d6cb04ecf33ef8a8661e6facd
yantonsoup <ygoldobin@gmail.com>
2016-12-15 09:14:22 -0800

    configure sass setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
- 
+var css = require("!style-loader!css-loader!sass-loader!./file.scss");
+
 module.exports = {
   entry: './lib/index.jsx',
   output: { path: __dirname, filename: 'bundle.js' },
