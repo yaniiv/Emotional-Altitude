0d3f8b10439f1e1062b8aae9df8197a5c24b833d
yantonsoup <ygoldobin@gmail.com>
2016-12-21 15:11:19 -0800

    quick commit

--- lib/pages/home/input-block.jsx
+++ lib/pages/home/input-block.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import MySlider  from "./slider.jsx";
+import MySlider  from "./slider.jsx";
 import store from '../../store.js';
 import * as types from '../../actions/action-types.js';
 
@@ -14,51 +14,57 @@ export default class InputBlock extends React.Component {
     this.descriptionStyle = { width: '400px', height:'150px' };
     
     this.handleCommentSubmit = this.handleCommentSubmit.bind(this)
-    this.handleSliderChange = this.handleSliderChange.bind(this)
-    this.state = { sliderValue: 0 } 
+    // this.handleSliderChange = this.handleSliderChange.bind(this)
+    this.state = {
+      sliderValue: 0,
+      selectedDate: new Date()
+    } 
   }
   
   handleCommentSubmit(e) {
-    e.preventDefault();
-        console.log("hi")
-        console.log(this.state.sliderValue)
+        e.preventDefault();
+        console.log(e.target.childNodes)
 
-    this.state.sliderValue
-    store.dispatch({
-      type: types.ADD_DATA,
-      feelsObj: {
-        feelingNum: this.state.sliderValue,
-        feelingTagline: e.target.elements[0].value,
-        feelingDescriptiom: e.target.elements[0].value,
-        feelingDate: new Date()
-      }
-    })
+        console.log(parseInt(e.target.childNodes[0].childNodes[0].childNodes[3].innerText))
+
+    // console.log(e.target.childNodes[0].elements)
+        console.log("hi")
+        // console.log("sliderValue:", this.state.sliderValue)
+        // console.log("dateValue:", this.state.selectedDate)
+
+    // store.dispatch({
+    //   type: types.ADD_DATA,
+    //   feelsObj: {
+    //     feelingNum: this.state.sliderValue,
+    //     feelingTagline: e.target.elements[0].value,
+    //     feelingDescriptiom: e.target.elements[0].value,
+    //     feelingDate: this.state.selectedDate
+    //   }
+    // })
   }
 
-  handleSliderChange(nextVal){
-    this.setState({sliderValue: nextVal})
+  // handleSliderChange(nextVal){
+  //   this.setState({sliderValue: nextVal})
+  // }
+
+  handleDateChange(newDate) {
+    this.setState({selectedDate: newDate})
   }
 
   render () {
     return (
       <div id="inputBlock">
-        <div id="input-slider"> 
-        <label className="pt-label .modifier">
-          What Is Your Emotional Altitude?  
-         <Slider
-          min={0}
-          max={100}
-          stepSize={1}
-          labelStepSize={10}
-          value={this.state.sliderValue}
-          onChange={(value)=>{this.handleSliderChange(value)}}
-          />          
-          </label>
-        </div>  
+        
 
-      <DatePicker/>  
+        <div id ="input-bottom-section">
 
-      <form id="input-form" onSubmit={this.handleCommentSubmit}>
+   
+
+          <form id="input-form" onSubmit={this.handleCommentSubmit}>
+          
+                  <MySlider />
+
+
         <label className="pt-label .modifier">
           Emotion Tagline
           <input className="pt-input" type="text" placeholder="Text input" dir="auto" />
@@ -70,13 +76,35 @@ export default class InputBlock extends React.Component {
         </label>
 
         <input type="submit" value="Submit State" />
-        </form>
+          </form>
 
+             <DatePicker
+          id ="input-date"
+          value={this.state.selectedDate}
+          onChange={(newDate) => {this.handleDateChange(newDate)}}
+        />    
+
+          </div>
 
     </div> 
     );
   }
 };
+
+        // <div id="input-slider"> 
+        //   <label className="pt-label .modifier">
+        //     What Is Your Emotional Altitude?    
+        //   <Slider
+        //     min={0}
+        //     max={100}
+        //     stepSize={1}
+        //     labelStepSize={10}
+        //     value={this.state.sliderValue}
+        //     onChange={(value)=>{this.handleSliderChange(value)}}
+        //   />          
+        //   </label>
+        // </div>
+
 
 
         //  <input
