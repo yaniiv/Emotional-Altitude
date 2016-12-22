7508b91e89a90e0625a0363005d534e956ae4e24
yantonsoup <ygoldobin@gmail.com>
2016-12-21 17:36:17 -0800

    positioning fix

--- lib/pages/home/form-text.jsx
+++ lib/pages/home/form-text.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
 
 const MyForm = (props) => {
+  var taglineStyle = { width: '400px' };
+  var descriptionStyle = {width: '400px', height: '150px', resize: "none" };
+  
   return (
     <form id="input-form" onSubmit={props.handleCommentSubmit}>
-        
+
       <label className="pt-label .modifier">
         Emotion Tagline
-        <input className="pt-input" type="text" placeholder="Text input" dir="auto" />
-      </label>
-        
-      <label className="pt-label .modifier">
-        Description
-        <input className="pt-input" type="text" placeholder="Input group" dir="auto" />
+        <input
+          className="pt-input"
+          type="text"
+          placeholder="Your General Vibe"
+          dir="auto"/>
       </label>
 
-          <input type="submit" value="Submit State" />
-        </form>
+      <label className="pt-label .modifier">
+        Description
+        <textarea
+          style={descriptionStyle}
+          className="pt-input .modifier"
+          dir="auto">
+      </textarea>
+      </label>
+
+      <input
+        className="pt-button pt-intent-primary"
+        type="submit"
+        value="Submit State"/>
+    </form>
   )
 }
 
-export default MyForm; 
+export default MyForm;
